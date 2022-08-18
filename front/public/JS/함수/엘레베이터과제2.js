25d704819d38109fd677466608ac52fe41d8c183
504 <504@DESKTOP-SOP80S8>
2022-08-18 18:46:20 +0900

    엘베과제2

--- front/public/JS/함수/엘레베이터과제2.js
+++ front/public/JS/함수/엘레베이터과제2.js
@@ -1,7 +1,7 @@
 // 엘베 그리기
 let btnlist = ''
 let 엘베 = ''
-let 현재엘베 = 1
+let 현재엘베 = ''
 function btn_create(){
 	btnlist= ''
 	for(let i = 5 ; i>=1 ; i--) {
@@ -14,28 +14,25 @@ function btn_create(){
 function btn_click(i) {
 //	let index = floor.indexOf(i)
 	btn_create()
+	현재엘베 = ''
 	// 현재 위치에서 상승 할때
 	if (엘베<=i) {
-		for(let 엘베 = 현재엘베 ; 엘베<=i ; 엘베++) {
+		for(let 엘베 = 현재엘베 ; 엘베<i ; 엘베++) {
 			alert(`${i}층입니다.`)
-		if(엘베<i) {
-			if( 엘베 == i ){
-				alert(`${i}층에 도착했습니다.`)
-				현재엘베=엘베
-			}
+		if( 엘베 == i-1 ){ // 위에 엘베<=i 하면 1층 더 많이 떠서 -1 해주었음
+			alert(`${i}층에 도착했습니다.`)
+			현재엘베=엘베
 		}
 		document.getElementById('btnbox'+(i)).innerHTML = '엘베위치'
 		}
 	}
 	else {
-		for(let 엘베 = 현재엘베 ; 엘베>=i ; 엘베--) {
+		for(let 엘베 = 현재엘베 ; 엘베>i ; 엘베--) {
 			alert(`${i}층입니다.`)
-		if(엘베>i) {
-			if( 엘베 == i-1 ){
+			if( 엘베 == i ){
 				alert(`${i}층에 도착했습니다.`)
 				현재엘베=엘베
 			}
-		}
 		document.getElementById('btnbox'+(i)).innerHTML = '엘베위치'
 		}
 	}
