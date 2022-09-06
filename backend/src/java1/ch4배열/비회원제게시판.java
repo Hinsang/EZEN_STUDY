723ac2d2fea00e265d3dc68ab379ce2d20b4b73c
황인상 <star4381@naver.com>
2022-09-06 19:56:28 +0900

    비회원제게시판

--- backend/src/java1/ch4배열/비회원제게시판.java
+++ backend/src/java1/ch4배열/비회원제게시판.java
@@ -16,11 +16,11 @@ public class 비회원제게시판 {
 //				
 //			}
 			System.out.println("-------------커뮤니티------------");
-			System.out.println("번호		작성자	제목");
+			System.out.println("번호	작성자	제목");
 			// 처음 시작할때 리스트들을 출력한다. 없으니까 출력안됨
 			for(int i = 0 ; i<boardlist.length ; i++) {
 				if(boardlist[i][0] != null) {						
-					System.out.println(i + "\t\t" + boardlist[i][2] + "\t\t" + boardlist[i][0]);
+					System.out.println(i + "\t" + boardlist[i][2] + "\t" + boardlist[i][0]);
 				}
 			}
 			
@@ -49,12 +49,12 @@ public class 비회원제게시판 {
 							boardlist[row][3] = password;
 							
 							System.out.println("-------------커뮤니티------------");
-							System.out.println("번호		작성자	제목");
+							System.out.println("번호	작성자	제목");
 							
 								// 총 배열의 값들을 출력한다.
 								if(boardlist[row][0] != null) {						
 									for(int i = 0 ; i<=row ; i++) {						
-										System.out.println(i + "\t\t" + boardlist[i][2] + "\t\t" + boardlist[i][0]);
+										System.out.println(i + "\t" + boardlist[i][2] + "\t" + boardlist[i][0]);
 									}
 								}
 	//					System.out.println("title : " + boardlist[row][0]);
@@ -68,40 +68,60 @@ public class 비회원제게시판 {
 					for(int row = 0 ; row<boardlist.length ; row++) { // 총 배열의 길이까지 반복한다.
 						if(boardlist[row][0] == null) { // 각 배열의 값이 비어있다면
 							System.out.println("---------------게시물 상세페이지--------------");
-							for(int i = 0 ; i<row ; i++) { // i부터 현재있는배열까지 출력한다.
-								System.out.print("작성자 : " + boardlist[i][2] + "\t\t");
-								System.out.println("제목 : " + boardlist[i][0]);
-								System.out.println("내용 : " + boardlist[i][1]);
-							}
+							
+							System.out.println("게시물 번호(인덱스) 선택 : ");
+							int boardpick = scanner.nextInt();
+							
+							System.out.print("작성자 : " + boardlist[boardpick][2] + "\t\t");
+							System.out.println("제목 : " + boardlist[boardpick][0]);
+							System.out.println("내용 : " + boardlist[boardpick][1]);
+									
 							System.out.println("1.목록보기(뒤로가기) 2.글삭제 3.글수정 선택 : ");
 							int btn2 = scanner.nextInt(); // btn2를 받는다.
 							if( btn2 == 1 ) { // btn2가 1이라면 뒤로가기
 								break;
 							}
 							
-							else if( btn2 == 2 ) {
-								for(int i = 0 ; i<boardlist.length ; i++) {
-								// --------------------------------------------//
-									System.out.println("알림)) 현 게시물 비밀번호 : ");
-									String passwordcheck = scanner.next();
-									if (boardlist[boardlist.length][3].equals(passwordcheck)) {										
-										boardlist[boardlist.length][0] = null;
-										boardlist[boardlist.length][1] = null;
-										boardlist[boardlist.length][2] = null;
-										boardlist[boardlist.length][3] = null;
-									}
-									// 삭제된 회원 뒤로 한칸씩 당기기
-									for( int j = i ; j<boardlist.length ; j++ ) { // j에 i넣고 배열의 길이까지 반복
-										boardlist[j][0] = boardlist[j+1][0];
-										boardlist[j][1] = boardlist[j+1][1];
-										boardlist[j][2] = boardlist[j+1][2];
-										boardlist[j][3] = boardlist[j+1][3];
-										if( boardlist[j+i][0] == null ) break; // 다음배열에 아이디 없으면 break
-									}
-									
+							else if( btn2 == 2 ) {	// 삭제
 								
+								System.out.println("알림)) 현 게시물 비밀번호 : ");
+								String passwordcheck = scanner.next();
+								if (boardlist[boardpick][3].equals(passwordcheck)) {										
+									boardlist[boardpick][0] = null;
+									boardlist[boardpick][1] = null;
+									boardlist[boardpick][2] = null;
+									boardlist[boardpick][3] = null;
+								
+									for(int i = boardpick ; i<boardlist.length-1 ; i++) { // 최대 인덱스 넘으면 에러나서 -1 해주었음
+										boardlist[i][0] = boardlist[i+1][0];
+										boardlist[i][1] = boardlist[i+1][1];
+										boardlist[i][2] = boardlist[i+1][2];
+										boardlist[i][3] = boardlist[i+1][3];
+											
+										if( boardlist[i+1] == null ) break;
+									}
+								} else {
+									System.out.println("잘못된 비밀번호 입니다.");
 								}
-							} else if( btn2 == 2 ) {
+										
+									break; // 다음배열에 아이디 없으면 break
+									
+							} else if( btn2 == 3 ) {	// 수정
+								System.out.println("알림)) 현 게시물 비밀번호 : ");
+								String passwordcheck = scanner.next();
+								
+								if(boardlist[boardpick][3].equals(passwordcheck)) {
+									System.out.print("수정할 제목 : ");
+									String title2 = scanner.next();
+									System.out.print("수정할 내용 : ");
+									String content2 = scanner.next();
+									
+									boardlist[boardpick][0] = title2;
+									boardlist[boardpick][1] = content2;
+									System.out.println("알림)) 내용이 수정되었습니다.");
+								} else {
+									System.out.println("잘못된 비밀번호 입니다.");
+								}
 								break;
 							}
 							break;
